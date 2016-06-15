0fd1d9fc24ceecb2a855d555ff4387c675a854c0
Kenny Westermark <kenny.westermark@itcompaniet.se>
2016-06-15 11:10:40 +0200

    UfOnline: MailTemplate

--- src/ITC.UnifaunOnline/Models/UnifaunUfOnline.cs
+++ src/ITC.UnifaunOnline/Models/UnifaunUfOnline.cs
@@ -19,24 +19,35 @@ namespace ITC.UnifaunOnline.Models
             [XmlAttribute("optid")]
             public string OptionId { get; set; }
 
+            /// <summary>
+            /// Text string with any contents. Line breaks can be entered with pipe character, |. 
+            /// </summary>
             [XmlIgnore] public string Message;
             [XmlIgnore] public string Cc;
             [XmlIgnore] public string Bcc;
             [XmlIgnore] public string SendMail;
             [XmlIgnore] public string From;
             [XmlIgnore] public string To;
+            /// <summary>
+            /// Mailtemplate. Used for ”Anpassad föravisering”
+            /// </summary>
+            [XmlIgnore] public string MailTemplate;
         }
     }
 
     public static class Options
     {
-        public static UnifaunUfOnline.UnifaunOption ENot(string to, string message)
+        public static UnifaunUfOnline.UnifaunOption ENot(string to, string message, string from, string mailTemplate = null, string cc = null, string bcc = null)
         {
             return new UnifaunUfOnline.UnifaunOption
             {
                 OptionId = "ENOT",
                 To = to,
-                Message = message
+                Message = message,
+                From = from,
+                Cc = cc,
+                Bcc = bcc,
+                MailTemplate = mailTemplate
             };
         }
     }
